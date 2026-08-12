ed59f01492c9f85b59ab2bf1cb7805f42f417fb6
asscontal-contabilidade <asscontal@asscontal.com.br>
2026-08-12 10:37:43 -0300

    blog: conecta painel ao Cloudflare Worker de produção

--- blog/assets/js/config.js
+++ blog/assets/js/config.js
@@ -1,5 +1,4 @@
 // Configuração do CMS
-// Depois de publicar o Cloudflare Worker, substitua a URL abaixo pela URL dele.
 window.BLOG_CONFIG = {
-  apiBase: "https://SEU-WORKER.workers.dev"
+  apiBase: "https://asscontal-blog-cms.asscontal.workers.dev"
 };
